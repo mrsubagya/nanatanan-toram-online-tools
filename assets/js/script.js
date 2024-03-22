9bbac91766daa9918cf96d7a2895fff9b80d4bdc
mrsubagya <r.subagya133@gmail.com>
2024-03-22 14:27:18 +0700

    Update overflow

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,19 +9,25 @@ let treeRight = document.getElementById('tree-right');
 let gateLeft = document.getElementById('gate-left');
 let gateRight = document.getElementById('gate-right');
 
-window.addEventListener('scroll', function(){
+function parallax() {
     let value = window.scrollY;
 
-    stars.style.bottom = value + 0.05 + 'px';
-    moon.style.bottom = value + 0.01 + 'px';
+    stars.style.bottom = value * 0.05 + 'px';
+    moon.style.bottom = value * 0.01 + 'px';
     treeLeft.style.left = value * -0.5 + 'px';
     treeRight.style.left = value * 0.5 + 'px';
     gateLeft.style.left = value * 0.4 + 'px';
     gateRight.style.left = value * -0.4 + 'px';
     text.style.right = -130 + value * 0.6 + '%';
     text.style.bottom = 100 + value * 0.01 + '%';
-    
-})
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    parallax();
+}
+
+animate();
 
 // Toggle class active
 const navigation = document.querySelector('.navigation');
